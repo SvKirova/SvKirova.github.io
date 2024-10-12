89259ccda1220231793ee37bf966bd3229de70fa
SvKirova <kiro0003@hz.nl>
2024-10-13 01:45:53 +0200

    Added comments to index.html page and organized index-style.css file (renamed index-css.css and sidenav.js and changed the ids of the elements in both files)

---
+++ asideMenu.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("myAsideMenu").style.width = "250px";
+}
+
+function closeMenu() {
+    document.getElementById("myAsideMenu").style.width = "0";
+}
